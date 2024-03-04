d101aba14a98b24993ed2b71d77cd4ca7e856cb1
Reza Nematpour <75791535+rezanematpour@users.noreply.github.com>
2024-03-04 06:07:50 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ setup(
     author='Fernando Marcos Wittmann',
     #author_email='fernando.wittmann[at]gmail[dot]com',
     description='A Python tool to rank Google Scholar publications by citations.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     url='https://github.com/rezanematpour/lbb',
     py_modules=['reza'],  # Assuming your script is named reza.py
     install_requires=[
@@ -18,8 +16,6 @@ setup(
             'reza=reza:main',  # This line sets up the command line tool
         ],
     },
-    classifiers=[
-        'Programming Language :: Python :: 3',
     ],
     python_requires='>=3.6',
 )
